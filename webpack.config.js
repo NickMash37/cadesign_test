2e748ff19e36add062f81f8c4676b156bf5d89ec
Nikolay <areyfclm@gmail.com>
2024-03-12 11:54:56 +0300

    initial dev commit

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './script/index.js',
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
