3a8bd981981c4d64e007cbcbc14026f2b62b30bd
Nikolay <areyfclm@gmail.com>
2024-03-12 13:06:00 +0300

    header is done

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './script/index.js',
+  entry: {
+    main: './script/index.js'
+  },
   output: {
     filename: 'main.bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [new HtmlWebpackPlugin()]
 };
